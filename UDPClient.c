0d29ab0bc374067c24e4335a603b5398b9968fbe
CrazyPhonk <barkhatov.egor@yandex.ru>
2024-03-20 15:10:53 +0700

    3213213

--- UDPClient.c
+++ UDPClient.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "002.h"
 #define PORT 8080
 
 void error(char *msg) {
@@ -17,12 +18,18 @@ int main() {
     FILE *file_ptr;
     int sockfd, connfd;
     struct sockaddr_in servaddr = {0};
-
     file_ptr = fopen("002.wav", "rb");
     if (file_ptr == NULL) {
         error("Error opening file");
     }
-   
+    fseek(file_ptr, 40, SEEK_SET); // Перейти к 40 байту в файле, где находится информация о количестве отсчетов
+    uint32_t dataSize;
+    //fread(&dataSize, sizeof(uint32_t), 1, file_ptr); // Прочитать количество байт данных
+   // printf("Datasize: %u\n", dataSize);
+    int sampleSize = 16; // Размер одного отсчета в битах (здесь предполагается, что это короткий int)
+    int sampleCount = dataSize * 8 / sampleSize; // Посчитать количество отсчетов
+    //printf("Total samples in the wav file: %d\n", sampleCount);
+    //printf("%ld\n", ftell(file_ptr));
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd == -1) {
         error("Error creating socket");
@@ -40,12 +47,15 @@ int main() {
     int i =0;
     int bytes_read;
     char c;
-    char buffer[1024]= {0};
-    char buf[1] = {0};
-    while ((c = fgetc(file_ptr)) != EOF) {
-        buffer[i] = c;
-        buf[0] = c;
-        printf("%d ", buffer[i]);
+    //char str[256] = "1233214214";
+   // short buffer[5]= {10000,20000,30000,-32000,30500};
+   // char* symb = (char *)&buffer[0];
+    unsigned char buf[2] = {0};
+    //fseek(file_ptr, 44, SEEK_SET); // Skip the header of the wav file (44 bytes)
+   // send(sockfd, symb, 10, 0);
+    while (_002_raw[i] != 0) {
+       // printf("%d ",_002_raw[i]);
+        buf[0] = _002_raw[i]; 
         i++;
         send(sockfd, buf, 1, 0);
         sleep(1);
